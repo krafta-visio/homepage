8f373e02e8e49e3c7fcbbdb2ffa0824de418f618
krafta. <krafta.visio@gmail.com>
2026-07-26 12:44:28 +0800

    asdf

--- protector.js
+++
@@ -1,8 +0,0 @@
-// protector.js - Safe console protection
-(function () {
-  try {
-    // Disable console logging if needed safely without breaking WebView thread loops
-  } catch (e) {
-    // Silent catch
-  }
-})();
